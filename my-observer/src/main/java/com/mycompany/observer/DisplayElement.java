0dbe1473e3a0c782b8b6fdc7f8a80dbe8105bf18
HardPlant <abc7988se@naver.com>
2018-11-13 01:58:58 +0900

    File separated

---
+++ my-observer/src/main/java/com/mycompany/observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package com.mycompany.observer;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ my-observer/src/main/java/com/mycompany/observer/Observer.java
@@ -0,0 +1,5 @@
+package com.mycompany.observer;
+
+public interface Observer{
+    public void update(float temp, float humidity, float pressure);
+}
