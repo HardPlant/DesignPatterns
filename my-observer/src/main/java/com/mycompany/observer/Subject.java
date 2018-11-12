7200aa8fa52511801b0bb835e9daa6adc6a35efb
HardPlant <abc7988se@naver.com>
2018-11-13 01:50:06 +0900

    A Subject Class

---
+++ my-observer/src/main/java/com/mycompany/observer/Subject.java
@@ -0,0 +1,15 @@
+package com.mycompany.observer;
+
+public interface Subject{
+    public void registerObserver (Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+}
+
+public interface Observer{
+    public void update(float temp, float humidity, float pressure);
+}
+
+public interface DisplayElement {
+    public void display();
+}
