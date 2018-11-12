9bfb13fabe1fd43a8e0db0a9ff8e929423ac4a63
HardPlant <abc7988se@naver.com>
2018-11-12 13:27:46 +0900

    added simulator, but it's buggy

---
+++ my-app/src/main/java/com/mycompany/app/MiniDuckSimulator.java
@@ -0,0 +1,8 @@
+package com.mycompany.app;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performFly();
+    }
+}
