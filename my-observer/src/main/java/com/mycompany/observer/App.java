07a6091afd5c616ee8d03619eedde38a43449d5f
HardPlant <abc7988se@naver.com>
2018-11-13 02:10:11 +0900

    it builds.

--- my-observer/src/main/java/com/mycompany/observer/App.java
+++ my-observer/src/main/java/com/mycompany/observer/App.java
@@ -12,5 +12,8 @@ public class App
 
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
         weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(45, 12, 27.4f);
+        weatherData.setMeasurements(28, 51, 34.4f);
+        weatherData.setMeasurements(37, 56, 30.4f);
     }
 }
