96f0e1f7bae9e5af18662680f1e463fa238501d4
HardPlant <abc7988se@naver.com>
2018-11-12 13:28:11 +0900

    now it works.

--- my-app/src/main/java/com/mycompany/app/Duck.java
+++ my-app/src/main/java/com/mycompany/app/Duck.java
@@ -6,7 +6,7 @@ public abstract class Duck{
     public Duck(){
 
     }
-    private void performFly(){
+    public void performFly(){
         flyBehavior.fly();
     }
     private interface QuackBehavior{
