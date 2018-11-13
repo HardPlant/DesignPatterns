de425a576132d4ddcf945cf15bf51465b7c45640
HardPlant <abc7988se@naver.com>
2018-11-13 10:53:16 +0900

    use it

--- my-observer/src/main/java/com/mycompany/observer/CurrentConditionsDisplay2.java
+++ my-observer/src/main/java/com/mycompany/observer/CurrentConditionsDisplay2.java
@@ -20,7 +20,9 @@ public class CurrentConditionsDisplay implements Observer{
 
     public void update(Observable obs, Object arg){
         if (obs instanceof WeatherData){
-
+            WeatherData weatherData = (WeatherData)obs;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
             display();
         }
     }
