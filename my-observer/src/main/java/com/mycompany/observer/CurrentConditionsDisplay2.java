42943cc5688ed4f76baee9ca0a35e8696c81288e
HardPlant <abc7988se@naver.com>
2018-11-13 10:49:07 +0900

    Let's work this

---
+++ my-observer/src/main/java/com/mycompany/observer/CurrentConditionsDisplay2.java
@@ -0,0 +1,26 @@
+package com.mycompany.observer;
+
+import com.mycompany.observer.Observer;
+import com.mycompany.observer.Subject;
+import com.mycompany.observer.DisplayElement;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement{
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData){
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure){
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+    public void display(){
+        System.out.println("Current coditions: " 
+            + temperature + " and " + humidity +" humidity ");
+    }
+}
