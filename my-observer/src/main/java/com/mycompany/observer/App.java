5fb743a459ef43ca0f9bab84d06473bc40411c84
HardPlant <abc7988se@naver.com>
2018-11-13 10:54:41 +0900

    Let's use

--- my-observer/src/main/java/com/mycompany/observer/App.java
+++ my-observer/src/main/java/com/mycompany/observer/App.java
@@ -8,9 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        WeatherData weatherData = new WeatherData();
+        WeatherDataOb weatherData = new WeatherData();
 
-        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        CurrentConditionsDisplay2 currentDisplay = new CurrentConditionsDisplay(weatherData);
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(45, 12, 27.4f);
         weatherData.setMeasurements(28, 51, 34.4f);
