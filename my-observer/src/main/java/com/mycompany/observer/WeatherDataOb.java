d16a0db77e427e2c97bec64ce05ee75b3b532e91
HardPlant <abc7988se@naver.com>
2018-11-13 10:47:16 +0900

    Remove self-write observer

--- my-observer/src/main/java/com/mycompany/observer/WeatherDataOb.java
+++ my-observer/src/main/java/com/mycompany/observer/WeatherDataOb.java
@@ -12,27 +12,6 @@ public class WeatherDataOb extends Observable {
     public WeatherData() {
     }
 
-    @Override
-    public void registerObserver(Observer o) {
-        observers.add(o);
-    }
-
-    @Override
-    public void removeObserver(Observer o) {
-        int i = observers.indexOf(o);
-        if (i >= 0) {
-            observers.remove(i);
-        }
-    }
-
-    @Override
-    public void notifyObservers() {
-        for (int i = 0; i < observers.size(); i++) {
-            Observer observer = (Observer) observers.get(i);
-            observer.update(temperature, humidity, pressure);
-        }
-    }
-
     public void measurementsChanged() {
         notifyObservers();
     }
