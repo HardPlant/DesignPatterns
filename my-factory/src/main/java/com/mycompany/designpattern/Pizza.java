de0b10603db3f8bffe6abfddd7d2ac9afa2d183a
HardPlant <abc7988se@naver.com>
2018-11-15 00:20:23 +0900

    피자 구현

--- my-factory/src/main/java/com/mycompany/designpattern/Pizza.java
+++ my-factory/src/main/java/com/mycompany/designpattern/Pizza.java
@@ -1,8 +1,26 @@
 package com.mycompany.designpattern;
 
-public interface Pizza{
-    public void prepare();
-    public void bake();
-    public void cut();
-    public void box();
+public abstract class Pizza{
+    String name;
+    String dough;
+    String sauce;
+    ArrayList toppings = new ArrayList();
+    
+    public void prepare(){
+        {
+            System.out.println(name + "준비");
+        }
+    }
+    public void bake(){
+        System.out.println("구워라");
+    }
+    public void cut(){
+        System.out.println("잘라라");
+    }
+    public void box(){
+        System.out.println("포장해라");
+    }
+    public String getName(){
+        return name;
+    }
 }
