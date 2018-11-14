7f51ae50495a7d025f93370b845a4229f8e23d51
HardPlant <abc7988se@naver.com>
2018-11-14 23:21:53 +0900

    read byte and return .toLowerCase

--- my-decorator/src/main/java/com/mycompany/designpattern/LowerCaseInputStream.java
+++ my-decorator/src/main/java/com/mycompany/designpattern/LowerCaseInputStream.java
@@ -11,9 +11,10 @@ public class LowerCaseInputStream extends FilterInputStream{
 
     @Override
     public int read() throws IOException {
-        return super.read();
+        int c = super.read();
+        return (c == -1 ? c : Character.toLowerCase((char)c));
     }
-    
+
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
         return super.read(b, off, len);
