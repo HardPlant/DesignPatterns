93baf4c0748c3ada9afa32a447eea454eff9662e
HardPlant <abc7988se@naver.com>
2018-11-14 23:02:34 +0900

    HouseBlend

---
+++ my-decorator/src/main/java/com/mycompany/designpattern/HouseBlend.java
@@ -0,0 +1,11 @@
+package com.mycompany.designpattern;
+
+public class HouseBlend extends Beverage{
+    public HouseBlend(){
+        description = "House Blend";
+    }
+    @Override
+    public double cost() {
+        return .89;
+    }
+}
