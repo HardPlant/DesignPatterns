963f1620aa8cddf49277f5304626df77d05f156a
HardPlant <abc7988se@naver.com>
2018-11-15 00:21:45 +0900

    뉴욕피자임

---
+++ my-factory/src/main/java/com/mycompany/designpattern/NYStyleCheesePizza.java
@@ -0,0 +1,11 @@
+package com.mycompany.designpattern;
+
+import com.mycompany.designpattern.Pizza;
+
+public class NYStyleCheesePizza extends Pizza{
+    public NYStyleCheesePizza(){
+        name = "뉴욕스타일 치즈피자";
+        dough = "굵음";
+        sauce = "마리나라 소스임";
+    }
+}
