e1a6e4498b325ecd54ecd0f94208c33cc0a68a1c
HardPlant <abc7988se@naver.com>
2018-11-14 23:31:13 +0900

    Well?

--- my-decorator/src/main/java/com/mycompany/designpattern/App.java
+++ my-decorator/src/main/java/com/mycompany/designpattern/App.java
@@ -1,19 +1,31 @@
 package com.mycompany.designpattern;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Beverage beverage = new Espresso();
         System.out.println(beverage.getDescription() + " $" + beverage.cost());
-        
+
         Beverage beverage2 = new HouseBlend();
         beverage2 = new Mocha(beverage2);
         System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
-        
+
+        int c;
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
