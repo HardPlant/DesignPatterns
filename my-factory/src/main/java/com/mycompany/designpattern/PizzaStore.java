adf897b1024ed94873fe4a5cda2d0169c565e773
HardPlant <abc7988se@naver.com>
2018-11-14 23:57:16 +0900

    PizzaStore

--- my-factory/src/main/java/com/mycompany/designpattern/PizzaStore.java
+++ my-factory/src/main/java/com/mycompany/designpattern/PizzaStore.java
@@ -1,5 +1,24 @@
 package com.mycompany.designpattern;
 
+import com.mycompany.designpattern.Pizza;
+import com.mycompany.designpattern.SimplePizzaFactory;
+
 public class PizzaStore{
-    
+    SimplePizzaFactory factory;
+    public PizzaStore(SimplePizzaFactory factory){
+        this.factory = factory;
+    }
+
+    public Pizza orderPizza(String type){
+        Pizza pizza;
+
+        pizza = factory.createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        
+        return pizza;
+    }
 }
