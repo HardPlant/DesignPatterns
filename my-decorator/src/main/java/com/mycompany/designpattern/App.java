847ea2f61ede681c1877e88da83ea44fdb720e31
HardPlant <abc7988se@naver.com>
2018-11-14 23:06:30 +0900

    Let's do this

--- my-decorator/src/main/java/com/mycompany/designpattern/App.java
+++ my-decorator/src/main/java/com/mycompany/designpattern/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+        
+        Beverage beverage2 = new HouseBlend();
+        beverage2 = new Mocha(beverage2);
+        System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+        
     }
 }
