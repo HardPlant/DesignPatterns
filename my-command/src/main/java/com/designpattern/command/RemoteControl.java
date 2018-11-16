2649626764efe5302d33a7a9e1573477c3438410
HardPlant <abc7988se@naver.com>
2018-11-17 01:50:01 +0900

    and button press

--- my-command/src/main/java/com/designpattern/command/RemoteControl.java
+++ my-command/src/main/java/com/designpattern/command/RemoteControl.java
@@ -20,11 +20,11 @@ public class RemoteControl{
         onCommands[slot]=onCommand;
         offCommands[slot]=offCommand;
     }
-    public void onButtonWasPushed(){
-        
+    public void onButtonWasPushed(int slot){
+        onCommands[slot].execute();
     }
-    public void offButtonWasPushed(){
-
+    public void offButtonWasPushed(int slot){
+        offCommands[slot].execute();
     }
     @Override
     public String toString() {
