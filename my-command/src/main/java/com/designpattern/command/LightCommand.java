c3fd9fc7c1aecffa4f4f1cb2faff51bd38018c76
HardPlant <abc7988se@naver.com>
2018-11-17 01:20:49 +0900

    Implement it

---
+++ my-command/src/main/java/com/designpattern/command/LightCommand.java
@@ -0,0 +1,13 @@
+package com.designpattern.command;
+
+import com.designpattern.command.Command;
+
+public class LightOnCommand implements Command{
+    Light light;
+    public LightOnCommand(Light light){
+        this.light = light;
+    }
+    public void execute(){
+        light.on();
+    }
+}
