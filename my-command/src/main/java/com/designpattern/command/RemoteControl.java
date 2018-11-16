2f94ad6d54be620e726975fed959fbc8975d8575
HardPlant <abc7988se@naver.com>
2018-11-17 02:06:54 +0900

    typo

--- my-command/src/main/java/com/designpattern/command/RemoteControl.java
+++ my-command/src/main/java/com/designpattern/command/RemoteControl.java
@@ -29,7 +29,7 @@ public class RemoteControl{
     @Override
     public String toString() {
         StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("\n-----Remote Control------");
+        stringBuffer.append("\n-----Remote Control------\n");
         for(int i=0; i<onCommands.length;i++){
             stringBuffer.append("[slot"+i+"] "+onCommands[i].getClass().getName()
                 +   "   "+ offCommands[i].getClass().getName() + "\n");
