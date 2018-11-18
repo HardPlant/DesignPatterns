6b6ead9a5333cefe22b221e8e07cbdda56a0a80c
HardPlant <abc7988se@naver.com>
2018-11-19 01:46:44 +0900

    not needed, just for compile

--- my-ite-compo/src/main/java/com/designpattern/ite-compo/PancakeHouseMenuIterator.java
+++ my-ite-compo/src/main/java/com/designpattern/ite-compo/PancakeHouseMenuIterator.java
@@ -5,11 +5,9 @@ import java.util.Iterator;
 
 public class PancakeHouseMenuIterator implements com.designpattern.itecompo.Iterator{
     ArrayList items;
-    Iterator iterator;
 
     public PancakeHouseMenuIterator(ArrayList items){
         this.items = items;
-        iterator = items.iterator();
     }
     public boolean hasNext(){
         return iterator.hasNext();
