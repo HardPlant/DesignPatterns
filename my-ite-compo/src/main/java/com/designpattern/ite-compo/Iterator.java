25894217a38a672c6b838b9263d64b4e57410798
HardPlant <abc7988se@naver.com>
2018-11-19 01:44:06 +0900

    changed built Iterator to Menu

--- my-ite-compo/src/main/java/com/designpattern/ite-compo/Iterator.java
+++
@@ -1,6 +0,0 @@
-package com.designpattern.itecompo;
-
-public interface Iterator{
-    boolean hasNext();
-    Object next();
-}
---
+++ my-ite-compo/src/main/java/com/designpattern/ite-compo/Menu.java
@@ -0,0 +1,7 @@
+package com.designpattern.itecompo;
+
+import java.util.Iterator;
+
+public interface Menu{
+    public Iterator createIterator();
+}
