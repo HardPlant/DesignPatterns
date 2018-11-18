ba2a5292239151010375f30185ded8826116c765
HardPlant <abc7988se@naver.com>
2018-11-19 00:41:15 +0900

    array impl

---
+++ my-ite-compo/src/main/java/com/designpattern/ite-compo/DinerMenu.java
@@ -0,0 +1,43 @@
+package com.designpattern.itecompo;
+
+public class DinerMenu{
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    MenuItem[] menuItems;
+
+    public DinerMenu(){
+        menuItems = new MenuItem[MAX_ITEMS];
+
+        addItem("BLT1",
+        "Desc",
+        true
+        ,2.99);
+        addItem("BLT2",
+        "Desc",
+        true
+        ,2.99);
+        addItem("BLT3",
+        "Desc",
+        true
+        ,2.99);
+        addItem("BLT4",
+        "Desc",
+        true
+        ,2.99);
+    }
+    public void addItem(String name, String description, boolean vegetarian, double price){
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if(numberOfItems >= MAX_ITEMS){
+            System.err.println("menu is Full");
+        } else {
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems = numberOfItems + 1;
+        }
+    }
+    /**
+     * @return the menuItems
+     */
+    public MenuItem[] getMenuItems() {
+        return menuItems;
+    }
+}
