e43b7f6bdb73ff4a24147c936bc7f37f9904a41f
HardPlant <abc7988se@naver.com>
2018-11-18 14:59:31 +0900

    lets build

--- my-adapter/src/main/java/com/designpattern/adapter/App.java
+++ my-adapter/src/main/java/com/designpattern/adapter/App.java
@@ -9,5 +9,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        MallardDuck duck = new MallardDuck();
+        
+        WildTurkey turkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+        
+        System.out.println("Turkey says..");
+        turkey.qubble();
+        turkey.fly();
+        System.out.println("Duck says..");
+        testDuck(duck);
+        
+        System.out.println("TurkeyAdapter says..");
+        testDuck(turkeyAdapter);
     }
+    static void testDuck(Duck duck){
+        duck.quack();
+        duck.fly();
+    }
+
 }
