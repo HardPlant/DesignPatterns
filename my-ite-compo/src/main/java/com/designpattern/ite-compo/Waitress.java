fc5137c080f2644a63d9d41107203d1ed7c14b05
HardPlant <abc7988se@naver.com>
2018-11-19 11:29:30 +0900

    it look like this.

--- my-ite-compo/src/main/java/com/designpattern/ite-compo/Waitress.java
+++ my-ite-compo/src/main/java/com/designpattern/ite-compo/Waitress.java
@@ -3,22 +3,14 @@ package com.designpattern.itecompo;
 import java.util.Iterator;
 
 public class Waitress{
-    Menu pancakeHouseMenu;
-    Menu dinerMenu;
-    Menu cafeMenu;
-    public Waitress(Menu pancakeHouseMenu, Menu dineMenu, Menu cafeMenu){
-        this.pancakeHouseMenu = pancakeHouseMenu;
-        this.dinerMenu = dineMenu;
-        this.cafeMenu = cafeMenu;
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus){
+        this.allMenus = allMenus;
     }
 
     public void printMenu(){
-        Iterator pancakeIterator = pancakeHouseMenu.createIterator();
-        Iterator dinerIterator = dinerMenu.createIterator();
-        Iterator cafeIterator = cafeMenu.createIterator();
-        printMenu(pancakeIterator);
-        printMenu(dinerIterator);
-        printMenu(cafeIterator);
+        allMenus.print();
     }
     private void printMenu(Iterator iterator){
         while(iterator.hasNext()){
