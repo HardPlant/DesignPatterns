229642554c9f57a968174b580fad81172a1a4423
HardPlant <abc7988se@naver.com>
2018-11-19 13:14:24 +0900

    implement Iterator

--- my-ite-compo/src/main/java/com/designpattern/ite-compo/CompositeIterator.java
+++ my-ite-compo/src/main/java/com/designpattern/ite-compo/CompositeIterator.java
@@ -1,5 +1,24 @@
 package com.designpattern.itecompo;
 
-public class CompositeIterator{
-    
+import java.util.Iterator;
+import java.util.Stack;
+
+public class CompositeIterator implements Iterator{
+    Stack<E> stack = new Stack();
+
+    public CompositeIterator(Iterator iterator){
+        stack.push(iterator);
+    }
+    @Override
+    public Object next() {
+        return null;
+    }
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+    @Override
+    public void remove() {
+        Iterator.super.remove();
+    }
 }
