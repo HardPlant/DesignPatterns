935cb8c190978047d801f31f29f706c98ab66a25
HardPlant <abc7988se@naver.com>
2018-11-19 11:24:21 +0900

    now menu do things

--- my-ite-compo/src/main/java/com/designpattern/ite-compo/Menu.java
+++ my-ite-compo/src/main/java/com/designpattern/ite-compo/Menu.java
@@ -1,7 +1,43 @@
 package com.designpattern.itecompo;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Menu extends MenuComponent{
-    public Iterator createIterator();
+    ArrayList menuComponent = new ArrayList();
+    String name;
+    String description;
+
+    public Menu(String name, String description){
+        this.name=name;
+        this.description=description;
+    }
+    public void add(MenuComponent menuComponent){
+        menuComponent.add(menuComponent);
+    }
+    public void remove(MenuComponent menuComponent){
+        menuComponent.remove(menuComponent);
+    }
+    public MenuComponent getChild(int i){
+        return (MenuComponent)menuComponent.get(i);
+    }
+    
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+    public void print(){
+        System.out.print("\n"+getName());
+        System.out.println(", " + getDescription());
+        System.out.println("----------------------");
+
+    }
 }
