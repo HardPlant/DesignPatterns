c7c3da194e66433681a8da8125995d81ae2cb18e
HardPlant <abc7988se@naver.com>
2018-11-19 11:42:53 +0900

    typo, be ware it's tree

--- my-ite-compo/src/main/java/com/designpattern/ite-compo/Menu.java
+++ my-ite-compo/src/main/java/com/designpattern/ite-compo/Menu.java
@@ -4,29 +4,31 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Menu extends MenuComponent{
-    ArrayList menuComponent = new ArrayList();
+    ArrayList menuComponents = new ArrayList();
     String name;
     String description;
-
+    public Menu(){
+        
+    }
     public Menu(String name, String description){
         this.name=name;
         this.description=description;
     }
     public void add(MenuComponent menuComponent){
-        menuComponent.add(menuComponent);
+        menuComponents.add(menuComponent);
     }
     public void remove(MenuComponent menuComponent){
-        menuComponent.remove(menuComponent);
+        menuComponents.remove(menuComponent);
     }
     public MenuComponent getChild(int i){
-        return (MenuComponent)menuComponent.get(i);
+        return (MenuComponent)menuComponents.get(i);
     }
     public void print(){
         System.out.print("\n"+getName());
         System.out.println(", " + getDescription());
         System.out.println("----------------------");
 
-        Iterator iterator = menuComponent.iterator();
+        Iterator iterator = menuComponents.iterator();
         while(iterator.hasNext()){
             MenuComponent menuComponent = (MenuComponent)iterator.next();
             menuComponent.print();
