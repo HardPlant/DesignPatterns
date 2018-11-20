3d3213de314aeb526ffbe90aeb10c25edc952903
HardPlant <abc7988se@naver.com>
2018-11-20 13:34:03 +0900

    여기서 진짜 ImageIcon을 불러옴, 이미지아이콘을 불러오는 것과 동기적이고, ImageIcon 생성자는 이미지가 로드될 떄까지 반환되지 않음

--- my-proxy/src/main/java/com/designpattern/proxy/ImageProxy.java
+++ my-proxy/src/main/java/com/designpattern/proxy/ImageProxy.java
@@ -41,6 +41,7 @@ class ImageProxy implements Icon{
                     public void run() {
                         try{
                             imageIcon = new ImageIcon(imageURL, "CD Cover");
+                            //
                             c.repaint();
                         } catch(Exception e){
                             e.printStackTrace();
