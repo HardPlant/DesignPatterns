453e1b852d4dff00fd82728da610279d40dc4200
HardPlant <abc7988se@naver.com>
2018-11-20 15:14:05 +0900

    honk void

--- my-compound/src/main/java/com/designpattern/compound/Goose.java
+++ my-compound/src/main/java/com/designpattern/compound/Goose.java
@@ -5,7 +5,7 @@ import com.designpattern.compound.Quackable;
 public class Goose{
     public Goose(){
     }
-    public Quackable honk(){
+    public void honk(){
         
         System.out.println("Honk");
     }
