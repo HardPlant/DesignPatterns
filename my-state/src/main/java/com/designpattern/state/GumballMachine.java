2821de3057db6eff584f4f07be866227924fe143
HardPlant <abc7988se@naver.com>
2018-11-20 10:12:53 +0900

    a state machine

---
+++ my-state/src/main/java/com/designpattern/state/GumballMachine.java
@@ -0,0 +1,30 @@
+package com.designpattern.state;
+
+public class GumballMachine {
+    final static int SOLD_OUT = 0;
+    final static int NO_QUARTER = 1;
+    final static int HAS_QUARTER = 2;
+    final static int SOLD = 3;
+    int state = SOLD_OUT;
+    int count = 0;
+
+    public GumballMachine(int count) {
+        this.count = count;
+        if (count > 0) {
+            state = NO_QUARTER;
+        }
+    }
+
+    public void insertQuarter() {
+        if (state == HAS_QUARTER) {
+            System.out.println("You can't insert another quarter");
+        } else if (state == SOLD_OUT) {
+            System.out.println("You can't inser a quarter, the machine is sold out");
+        } else if (state == SOLD) {
+            System.out.println("Please wait, we're already giving you a gumball");
+        } else if (state == NO_QUARTER) {
+            state = HAS_QUARTER;
+            System.out.println("You inserted a quarter");
+        }
+    }
+}
