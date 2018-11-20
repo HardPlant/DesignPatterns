50084fd21dd049b49a30004eb8ff044ae95b5ae9
HardPlant <abc7988se@naver.com>
2018-11-20 11:12:22 +0900

    HasQuarterState

---
+++ my-state/src/main/java/com/designpattern/state/HasQuarterState.java
@@ -0,0 +1,29 @@
+package com.designpattern.state;
+
+import com.designpattern.state.GumballMachine;
+
+public class HasQuarterState implements State{
+    GumballMachine gumballMachine;
+    public HasQuarterState(GumballMachine machine){
+        this.gumballMachine = machine;
+    }
+    @Override
+    public void insertQuarter() {
+        System.out.println("You can't insert another quarter");
+
+    }
+    @Override
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+
+    }
+    @Override
+    public void turnCrank() {
+        System.out.println("You turned...");
+        gumballMachine.dispense();
+    }
+    @Override
+    public void dispense() {
+        System.out.println("No gumball dispesed");
+    }
+}
