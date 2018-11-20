fb1cc70190176fc0b4a30fedb790bba57cef4c5a
HardPlant <abc7988se@naver.com>
2018-11-20 14:11:53 +0900

    a matchmaking service

---
+++ my-proxy/src/main/java/com/designpattern/proxy/PersonBean.java
@@ -0,0 +1,13 @@
+package com.designpattern.proxy;
+
+public interface PersonBean{
+    String getName();
+    String getGender();
+    String getInterest();
+    int getHotOrNotRating();
+
+    void setName(String name);
+    void setGender(StringgGender);
+    void setInterest(String interest);
+    void setHotOrNotRating(int rating);
+}
