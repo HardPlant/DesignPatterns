7bfd6eba0f2ebb1c7f33f6514b90ec03c5c5ace7
HardPlant <abc7988se@naver.com>
2018-11-21 21:10:57 +0900

    Observer Quack

---
+++ my-compound/src/main/java/com/designpattern/compound/QuackObservable.java
@@ -0,0 +1,8 @@
+package com.designpattern.compound;
+
+import java.util.Observer;
+
+public interface QuackObservable{
+    public void registerObserver(Observer observer);
+    public void notifyObserver();
+}
--- my-compound/src/main/java/com/designpattern/compound/Quackable.java
+++ my-compound/src/main/java/com/designpattern/compound/Quackable.java
@@ -1,5 +1,5 @@
 package com.designpattern.compound;
 
-public interface Quackable{
+public interface Quackable extends QuackObservable{
     public void quack();
 }
