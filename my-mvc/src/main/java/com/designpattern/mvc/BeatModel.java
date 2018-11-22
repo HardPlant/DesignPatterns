af40cbcb7a3718192833c636b693d7320798f8a7
HardPlant <abc7988se@naver.com>
2018-11-23 06:18:02 +0900

    옵저버 기능 추가

--- my-mvc/src/main/java/com/designpattern/mvc/BeatModel.java
+++ my-mvc/src/main/java/com/designpattern/mvc/BeatModel.java
@@ -3,6 +3,7 @@ package com.designpattern.mvc;
 import java.util.ArrayList;
 
 import javax.sound.midi.MetaEventListener;
+import javax.sound.midi.MetaMessage;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.Track;
@@ -40,5 +41,37 @@ public class BeatModel implements BeatModelInterface, MetaEventListener{
     }
     void beatEvent(){
         notifyBeatObservers();
+    }
+    public void registerObserver(BeatObserver o) {
+        beatObservers.add(o);
+    }
+    public void notifyBeatObservers(){
+        for(int i=0; i<beatObservers.size();i++){
+            BeatObserver observer = (BeatObserver)beatObservers.get(i);
+            observer.updateBeat();
+        }
+    }
+    
+    public void registerObserver(BPMObserver o) {
+        bpmObservers.add(o);
+    }
+
+    public void notifyBPMObservers(){
+        for(int i=0; i<bpmObservers.size(); i++){
+            BPMObserver observer = (BPMObserver)bpmObservers.get(i);
+            observer.updateBPM();
+        }
+    }
+    public void removeObserver(BeatObserver o) {
+        int i = beatObservers.indexOf(o);
+        if(i>=0){
+            beatObservers.remove(i);
+        }
+    }
+    public void removeObserver(BPMObserver o) {
+        int i = bpmObservers.indexOf(o);
+        if(i>=0){
+            bpmObservers.remove(i);
+        }
     }
 }
