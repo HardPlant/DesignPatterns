b9fd33fa9c182c05bbcc2d31410bb507bce6dd94
HardPlant <abc7988se@naver.com>
2018-11-23 06:06:33 +0900

    컨트롤러는 모델을 넘겨받아 뷰를 생성함

--- my-mvc/src/main/java/com/designpattern/mvc/BeatController.java
+++ my-mvc/src/main/java/com/designpattern/mvc/BeatController.java
@@ -4,4 +4,13 @@ public class BeatController implements ControllerInterface{
     BeatModelInterface model;
     DJView view;
 
+    public BeatController(BeatModelInterface model){
+        this.model = model;
+        view = new DJView(this, model);
+        view.createView();
+        view.createControls();
+        view.disableStopMenuItem();
+        view.enableStartMenuItem();
+        model.initialize();
+    }
 }
