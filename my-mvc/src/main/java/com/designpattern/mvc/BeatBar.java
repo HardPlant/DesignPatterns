a31f50496e13526ad7001be9de160db451cfa478
HardPlant <abc7988se@naver.com>
2018-11-23 06:36:57 +0900

    BeatBar with JProgressbar

---
+++ my-mvc/src/main/java/com/designpattern/mvc/BeatBar.java
@@ -0,0 +1,10 @@
+package com.designpattern.mvc;
+
+import javax.swing.JProgressBar;
+
+public class BeatBar extends JProgressBar{
+    public BeatBar(){
+        this.setMinimum(0);
+        this.setMaximum(100);
+    }
+}
