7dd53539e3f5ad717afee15e151404c734136a5a
HardPlant <abc7988se@naver.com>
2018-11-23 06:24:52 +0900

    create midi message and dd to track

--- my-mvc/src/main/java/com/designpattern/mvc/BeatModel.java
+++ my-mvc/src/main/java/com/designpattern/mvc/BeatModel.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 import javax.sound.midi.MetaEventListener;
 import javax.sound.midi.MetaMessage;
+import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
 import com.designpattern.mvc.BeatModelInterface;
@@ -95,5 +97,38 @@ public class BeatModel implements BeatModelInterface, MetaEventListener{
             e.printStackTrace();
         }
     }
-    
+    public void buildTrackAndStart(){
+        int[] trackList = {35, 0, 46, 0};
+        sequence.deleteTrack(null);
+        track = sequence.createTrack();
+
+        makeTracks(trackList);
+        track.add(makeEvent(192,9,1,0,4));
+        try{
+            sequencer.setSequence(sequence);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void makeTracks(int[] list){
+        for(int i=0; i<list.length; i++){
+            int key = list[i];
+
+            if(key != 0){
+                track.add(makeEvent(144, 9, key, 100, i));
+                track.add(makeEvent(128, 9, key, 100, i+1));
+            }
+        }
+    }
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
+        MidiEvent event = null;
+        try{
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return event;
+    }
 }
