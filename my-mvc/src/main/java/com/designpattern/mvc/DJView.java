d6b620006cb393091a97fc7476c496e709098267
HardPlant <abc7988se@naver.com>
2018-11-23 05:45:37 +0900

    생성자에서 컨트롤러 모델을 등록하고 모델 옵저버에 자신을 등록한다.

--- my-mvc/src/main/java/com/designpattern/mvc/DJView.java
+++ my-mvc/src/main/java/com/designpattern/mvc/DJView.java
@@ -1,5 +1,21 @@
 package com.designpattern.mvc;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class DJView implements ActionListener, BeatObserver, BPMObserver{
+    BeatModelInterface model;
+    ControllerInterface controller;
+    JFrame viewFrame;
+    JPanel viewPanel;
+    BeatBar beatBar;
+    JLabel bpmOutputLabel;
     
+    public DJView(ControllerInterface controller, BeatModelInterface model){
+        this.controller = controller;
+        this.model = model;
+        model.registerObserver((BeatObserver)this);
+        model.registerObserver((BPMObserver)this);
+    }
 }
