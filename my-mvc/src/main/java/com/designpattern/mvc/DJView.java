11b51a6f79a50a06ce4c024f1314c6c81572d743
HardPlant <abc7988se@naver.com>
2018-11-23 06:47:26 +0900

    controller view

--- my-mvc/src/main/java/com/designpattern/mvc/DJView.java
+++ my-mvc/src/main/java/com/designpattern/mvc/DJView.java
@@ -6,9 +6,11 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
@@ -27,13 +29,14 @@ public class DJView implements ActionListener, BeatObserver, BPMObserver{
     
     JFrame controlFrame;
     JPanel controlPanel;
+    JPanel insideControlPanel;
     JLabel bpmLabel;
     JTextField bpmTextField;
     JButton setBPMButton;
     JButton increaseBPMButton;
     JButton decreaseBPMButton;
     JMenuBar menuBar;
-    JMenuBar menu;
+    JMenu menu;
     JMenuItem startMenuItem;
     JMenuItem stopMenuItem;
 
@@ -72,7 +75,66 @@ public class DJView implements ActionListener, BeatObserver, BPMObserver{
     }
 
     public void createControls(){
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        controlFrame = new JFrame("Control");
+        controlFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        controlFrame.setSize(new Dimension(100,80));
 
+        controlPanel = new JPanel(new GridLayout(1, 2));
+
+        menuBar = new JMenuBar();
+        menu = new JMenu("DJ Control");
+        startMenuItem = new JMenuItem("Start");
+        startMenuItem.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                controller.start();
+            }
+        });
+        stopMenuItem = new JMenuItem("Stop");
+        menu.add(startMenuItem);
+
+        stopMenuItem.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                controller.stop();
+            }
+        });
+        menu.add(stopMenuItem);
+        JMenuItem exit = new JMenuItem("Quit");
+        exit.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        menu.add(exit);
+        menuBar.add(menu);
+
+        bpmTextField = new JTextField(2);
+        bpmLabel = new JLabel("Enter BPM:", SwingConstants.RIGHT);
+        setBPMButton = new JButton("Set");
+        setBPMButton.setSize(new Dimension(10,40));
+        increaseBPMButton = new JButton(">>");
+        decreaseBPMButton = new JButton("<<");
+        increaseBPMButton.addActionListener(this);
+        decreaseBPMButton.addActionListener(this);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(1,2));
+        buttonPanel.add(decreaseBPMButton);
+        buttonPanel.add(increaseBPMButton);
+
+        JPanel enterPanel = new JPanel(new GridLayout(3,1));
+        insideControlPanel.add(enterPanel);
+        insideControlPanel.add(setBPMButton);
+        insideControlPanel.add(buttonPanel);
+        controlPanel.add(insideControlPanel);
+
+        bpmLabel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        bpmOutputLabel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+
+        controlFrame.getRootPane().setDefaultButton(setBPMButton);
+        controlFrame.getContentPane().add(controlPanel, BorderLayout.CENTER);
+
+        controlFrame.pack();
+        controlFrame.setVisible(true);
     }
     public void enableStopMenuItem(){
         stopMenuItem.setEnabled(true);
