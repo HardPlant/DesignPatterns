4ed0dd4349beea34e7790fd9f42240085d08e3b1
HardPlant <abc7988se@naver.com>
2018-11-24 01:04:43 +0900

    adapter skeleton

---
+++ my-mvc/src/main/java/com/designpattern/mvc/HeartAdapter.java
@@ -0,0 +1,36 @@
+package com.designpattern.mvc;
+
+public class HeartAdapter implements BeatModelInterface{
+    HeartModelInterface heart;
+
+    public HeartAdapter(){
+
+    }
+    public void initialize() {
+        
+    }
+    public void on() {
+        
+    }
+    public void off() {
+        
+    }
+    public int getBPM() {
+        return 0;
+    }
+    public void setBPM(int bpm) {
+        
+    }
+    public void registerObserver(BeatObserver o) {
+        
+    }
+    public void removeObserver(BeatObserver o) {
+        
+    }
+    public void registerObserver(BPMObserver o) {
+        
+    }
+    public void removeObserver(BPMObserver o) {
+        
+    }
+}
