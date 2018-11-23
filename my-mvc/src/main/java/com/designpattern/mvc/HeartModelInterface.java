82009d217d5a7d2f7a91f4373c48dec4600a84dd
HardPlant <abc7988se@naver.com>
2018-11-24 01:06:50 +0900

    add removes

--- my-mvc/src/main/java/com/designpattern/mvc/HeartModelInterface.java
+++ my-mvc/src/main/java/com/designpattern/mvc/HeartModelInterface.java
@@ -3,5 +3,7 @@ package com.designpattern.mvc;
 public interface HeartModelInterface{
     public int getHeartRate();
     public void registerBeatObserver(BeatObserver o);
+    public void removeBeatObserver(BeatObserver o);
     public void registerBPMObserver(BPMObserver o);
+    public void removeBPMObserver(BPMObserver o);
 }
