ee20cb06c3865a42d7172bde6f3b36076caf92ac
HardPlant <abc7988se@naver.com>
2018-11-24 01:15:42 +0900

    어댑터 사용해서 DJView 생성

--- my-mvc/src/main/java/com/designpattern/mvc/HeartController.java
+++ my-mvc/src/main/java/com/designpattern/mvc/HeartController.java
@@ -1,5 +1,11 @@
 package com.designpattern.mvc;
 
 public class HeartController implements ControllerInterface{
-    
+    HeartModelInterface model;
+    DJView view;
+
+    public HeartController(HeartModelInterface model){
+        this.model = model;
+        view = new DJView(this, new HeartAdapter(model));
+    }
 }
